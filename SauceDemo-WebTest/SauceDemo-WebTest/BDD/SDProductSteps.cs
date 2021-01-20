6ae9f29d0ff2640922848a9fbe3adfe713bef72b
Kish Patel <KPatel2@spartaglobal.com>
2021-01-20 15:57:25 +0000

    Added Product POM

---
+++ SauceDemo-WebTest/SauceDemo-WebTest/BDD/SDProductSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SauceDemo_WebTest.BDD
+{
+    [Binding]
+    public class SDProductSteps
+    {
+        [Given(@"I am on the product page")]
+        public void GivenIAmOnTheProductPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I select sort by name A-Z")]
+        public void WhenISelectSortByNameA_Z()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the products are listed alphabetically")]
+        public void ThenTheProductsAreListedAlphabetically()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
