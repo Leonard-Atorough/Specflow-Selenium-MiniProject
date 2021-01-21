6a632b9ed5aa140221ad1f76c935acd076ec5495
Leonard Atorough <leotorough@gmail.com>
2021-01-21 10:24:38 +0000

    transferred to .Net framework

---
+++ SauceDemo-WebTest/SauceDemo_WebTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SauceDemo_WebTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SauceDemo_WebTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c61b5187-4ad8-4f10-b6cc-d6efaf3b15cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SauceDemo-WebTest/SauceDemo_WebTest/lib/Pages/SDCartPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+
+namespace SauceDemo_WebTest
+{
+    public class SDCartPage
+    {
+        private IWebDriver _seleniumDriver;
+
+        private string CartPageUrl = AppConfigReader.CartPageUrl;
+        private IWebElement _continueShopping => _seleniumDriver.FindElement(By.LinkText("Continue Shopping"));
+        private IReadOnlyList<IWebElement> _AnyFirstItem => _seleniumDriver.FindElements(By.ClassName("inventory_item_name"));
+
+
+        public SDCartPage(IWebDriver seleniumDriver)
+        {
+            _seleniumDriver = seleniumDriver;
+        }
+
+        public string GetAnyFirstItem() => _AnyFirstItem[0].Text;
+    }
+}
